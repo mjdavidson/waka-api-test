4876ca7052a766d6511453869f2958deb344ebfa
Matt Davidson <matt@mattdavidson.kiwi>
2019-07-01 17:31:38 +1000

    add realtime on stop view

--- src/stop.tsx
+++ src/stop.tsx
@@ -1,15 +1,19 @@
 import React, { Component, useState, useEffect } from 'react';
-
 import { withRouter, Link } from 'react-router-dom';
-import { RouteComponentProps } from 'react-router';
-import { StopProps, StopInfo, Trip } from './types';
-import TripItem from './trip';
+import { darken } from 'polished';
+import { StopProps, StopInfo, Trip, TripUpdate } from './types';
+import styled from 'styled-components';
+import { useInterval } from './useinterval';
 
 function Stop({ match }: StopProps) {
   const [info, setInfo] = useState<StopInfo>(null);
   const [infoLoading, setInfoLoading] = useState(false);
   const [trips, setTrips] = useState<Trip[]>([]);
   const [tripsLoading, setTripsLoading] = useState(false);
+  const [realtime, setRealtime] = useState<{ [tripId: string]: TripUpdate }>(
+    {}
+  );
+  const [now, setNow] = useState();
   useEffect(() => {
     const fetchData = async () => {
       setInfoLoading(true);
@@ -25,7 +29,31 @@ function Stop({ match }: StopProps) {
     };
     fetchData();
   }, [match.params.stopId]);
-  console.log(info, infoLoading);
+  useInterval(async () => {
+    getRealtime();
+  }, 15 * 1000);
+  const getRealtime = async () => {
+    const res = await fetch(`/au-syd/realtime`, {
+      method: 'POST',
+      body: JSON.stringify({ trips: trips.map(trip => trip.trip_id) }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const realtime = await res.json();
+    const now = new Date();
+    const then = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate(),
+      0,
+      0,
+      0
+    );
+    const diff = now.getTime() - then.getTime();
+    setNow(diff);
+    setRealtime(realtime);
+  };
   if (infoLoading) {
     return (
       <div>
@@ -34,6 +62,85 @@ function Stop({ match }: StopProps) {
     );
   }
   if (!infoLoading && !tripsLoading && info !== null && trips !== null) {
+    const blocks: { [blockId: string]: Trip[] } = {};
+    for (const trip of trips) {
+      if (Object.prototype.hasOwnProperty.call(blocks, trip.block_id)) {
+        blocks[trip.block_id].push(trip);
+      } else {
+        blocks[trip.block_id] = [trip];
+      }
+    }
+    const newTrips = trips
+      .filter(trip => trip.pickup_type === 0)
+      .map(trip => {
+        const r = realtime[trip.trip_id];
+        const split = trip.trip_id.split('.');
+        const departure = new Date(trip.departure_time_seconds * 1000);
+        let update;
+        let cancelled = false;
+        if (r) {
+          const rel = r.trip.scheduleRelationship;
+          if (rel === 'SCHEDULED') {
+            const found = r.stopTimeUpdate.find(
+              stu => stu.stopId === match.params.stopId
+            );
+            if (found) {
+              update = found;
+              departure.setSeconds(
+                departure.getSeconds() + found.departure.delay
+              );
+            }
+          }
+          if (rel === 'CANCELED') {
+            cancelled = true;
+          }
+        }
+
+        const departureTime = `${departure.getUTCHours()}:${departure
+          .getUTCMinutes()
+          .toString()
+          .padStart(2, '0')}:${departure
+          .getUTCSeconds()
+          .toString()
+          .padStart(2, '0')}`;
+        const arrival = new Date(trip.arrival_time_seconds * 1000);
+        const arrivalTime = `${arrival.getUTCHours()}:${arrival
+          .getUTCMinutes()
+          .toString()
+          .padStart(2, '0')}:${arrival
+          .getUTCSeconds()
+          .toString()
+          .padStart(2, '0')}`;
+        const status = cancelled
+          ? 'Cancelled'
+          : update
+          ? update.arrival.delay === 0
+            ? 'On time'
+            : update.departure.delay > 0
+            ? `${Math.ceil(update.departure.delay / 60)} minutes late`
+            : `${Math.ceil(
+                Math.abs(update.departure.delay / 60)
+              )} minutes early`
+          : '';
+        return {
+          ...trip,
+          departureTime,
+          arrivalTime,
+          status,
+          split,
+          arrival,
+          departure
+        };
+      })
+      .sort((a, b) => {
+        if (a.arrival_time_seconds < b.arrival_time_seconds) {
+          return -1;
+        }
+        if (a.arrival_time_seconds > b.arrival_time_seconds) {
+          return 1;
+        }
+        return 0;
+      });
     return (
       <div>
         <h1>{info.stop_name}</h1>
@@ -44,46 +151,33 @@ function Stop({ match }: StopProps) {
               <th>trip</th>
               <th>run number</th>
               <th>set</th>
-              <th>arriving</th>
+              {/* <th>arriving</th> */}
               <th>departing</th>
               <th>link</th>
+              <th>delay</th>
             </tr>
           </thead>
           <tbody>
-            {trips.map(trip => {
-              console.log(trip);
-              const split = trip.trip_id.split('.');
-              const departure = new Date(trip.departure_time_seconds * 1000);
-              const departureTime = `${departure.getUTCHours()}:${departure
-                .getUTCMinutes()
-                .toString()
-                .padStart(2, '0')}:${departure
-                .getUTCSeconds()
-                .toString()
-                .padStart(2, '0')}`;
-              const arrival = new Date(trip.arrival_time_seconds * 1000);
-              const arrivalTime = `${arrival.getUTCHours()}:${arrival
-                .getUTCMinutes()
-                .toString()
-                .padStart(2, '0')}:${arrival
-                .getUTCSeconds()
-                .toString()
-                .padStart(2, '0')}`;
+            {newTrips.map(trip => {
               return (
-                <tr>
+                <TripRow
+                  bgColor={trip.route_color}
+                  passed={trip.departure.getTime() < now}
+                >
                   <td>{trip.route_short_name}</td>
-                  <td>{trip.route_long_name}</td>
-                  <td>{split[0]}</td>
+                  <td>{trip.trip_headsign}</td>
+                  <td>{trip.split[0]}</td>
                   <td>
-                    {split[4]}
-                    {split[5]}
+                    {trip.split[4]}
+                    {trip.split[5]}
                   </td>
-                  <td>{arrivalTime}</td>
-                  <td>{departureTime}</td>
+                  {/* <td>{arrivalTime}</td> */}
+                  <td>{trip.departureTime}</td>
                   <td>
                     <Link to={`/trip/${trip.trip_id}`}>link</Link>
                   </td>
-                </tr>
+                  <td>{trip.status}</td>
+                </TripRow>
               );
             })}
           </tbody>
@@ -95,3 +189,9 @@ function Stop({ match }: StopProps) {
 }
 
 export default withRouter(Stop);
+
+const TripRow = styled.tr<{ bgColor: string; passed: boolean }>`
+  background-color: ${props =>
+    props.passed ? darken(0.1)(props.bgColor) : props.bgColor};
+  color: white;
+`;
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+import Long from 'long';
 import { RouteComponentProps } from 'react-router-dom';
 
 export type StopInfo = {
@@ -32,6 +33,9 @@ export interface Trip {
   stop_sequence: number;
   trip_headsign: string;
   trip_id: string;
+  block_id: string;
+  pickup_type: number;
+  drop_off_type: number;
 }
 
 export interface StopMatchParams {
@@ -58,6 +62,95 @@ export interface StopTime {
   route_short_name: string;
 }
 
+export interface extendedstoptime extends StopTime {
+  arrival?: number;
+  departure?: number;
+}
+
 export interface StopTimeItemProps {
-  stopTime: StopTime;
+  stopTime: extendedstoptime;
+}
+
+export interface UpdateFeedMessage {
+  entity: UpdateFeedEntity[];
+  header: FeedHeader;
+}
+
+export interface PositionFeedMessage {
+  entity: PositionFeedEntity[];
+  header: FeedHeader;
+}
+
+export interface PositionFeedEntity {
+  id: string;
+  vehicle: VehiclePosition;
+}
+
+export interface UpdateFeedEntity {
+  id: string;
+  tripUpdate: TripUpdate;
+}
+
+export interface TripUpdate {
+  stopTimeUpdate: StopTimeUpdate[];
+  timestamp: Long;
+  trip: TripDescriptor;
+  vehicle: VehicleDescriptor;
+}
+
+export interface StopTimeUpdate {
+  departure: StopTimeEvent;
+  arrival: StopTimeEvent;
+  scheduleRelationship: number;
+  stopId: string;
+  stopSequence: number;
+}
+
+export interface StopTimeEvent {
+  delay: number;
+  time: Long;
+}
+
+export interface TripDescriptor {
+  routeId: string;
+  scheduleRelationship: 'SCHEDULED' | 'CANCELED';
+  startDate: string;
+  startTime: string;
+  tripId: string;
+}
+
+export interface VehicleDescriptor {
+  id: string;
+  label: string;
+  licensePlate: string;
+}
+
+export interface FeedHeader {
+  gtfsRealtimeversion: string;
+  incrementality: number;
+  timestamp: Long;
+}
+
+export interface VehiclePosition {
+  congestionLevel: number;
+  position: {
+    latitude: number;
+    longitude: number;
+    bearing?: number;
+    speed?: number;
+    odometer?: number;
+  };
+  stopId: string;
+  timestamp: Long;
+  trip: TripDescriptor;
+  vehicle: VehicleDescriptor;
+}
+
+export interface RouteInfo {
+  route_short_name: string;
+  route_long_name: string;
+  route_desc: string;
+  route_type: number;
+  route_color: string;
+  route_text_color: string;
 }
